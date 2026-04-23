120fa33ec54c443edf358c9c54568520daff5ed0
Your Name <you@example.com>
2026-04-23 11:23:29 +0100

    fix environment

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://usermicroservice-omdq.onrender.com/api',
+  appointmentApiUrl: 'https://appointmentmicroservice.onrender.com/api',
+  doctorApiUrl: 'https://doctormicroservice.onrender.com/api',
+  patientApiUrl: 'https://patientmicroservice.onrender.com/api'
+};
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8086/api',
-  appointmentApiUrl: 'http://localhost:8087/api',
-  doctorApiUrl: 'http://localhost:8088/api',
-  patientApiUrl: 'http://localhost:8089/api'
-};
+    production: false,
+    apiUrl: 'http://localhost:8086/api',
+    appointmentApiUrl: 'http://localhost:8087/api',
+    doctorApiUrl: 'http://localhost:8088/api',
+    patientApiUrl: 'http://localhost:8089/api'
+  };
